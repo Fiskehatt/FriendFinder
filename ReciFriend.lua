8f0087e1107193b1354ab87f54c3520f3d4727ff
Coax <Fiskehatt@users.noreply.github.com>
2017-04-16 20:07:49 +0300

    Make the addon generic

--- ReciFriend.lua
+++ ReciFriend.lua
@@ -3,98 +3,27 @@
 
 local function addFriends()
 	DEFAULT_CHAT_FRAME:AddMessage("Adding your old pals!")	
-	for i, name in ipairs(ReciFriendsList) do
+	for i, name in ipairs(FriendsList) do
 		AddFriend(name)
 	end
 	DEFAULT_CHAT_FRAME:AddMessage("Finished!")
 	ToggleFriendsFrame(1)
 end
 
-SLASH_ReciFriend1 = "/recifriend"
-SlashCmdList["ReciFriend"] = ReciFriend_SlashCmdHandler
+SLASH_FriendFinder1 = "/friendfinder"
+SlashCmdList["FriendFinder"] = FriendFinder_SlashCmdHandler
 
-function ReciFriend_SlashCmdHandler(msg)
+function FriendFinder_SlashCmdHandler(msg)
 	addFriends()
 end
 
-function ReciFriend_OnLoad()
-	DEFAULT_CHAT_FRAME:AddMessage("Type /recifriend to add your old friends!")
+function FriendFinder_OnLoad()
+	DEFAULT_CHAT_FRAME:AddMessage("Type /friendfinder to add your old friends!")
 end
 
+-- Add your friends name here
 ReciFriendsList = {
-"Coax",
-"Eldarathian",
-"Jumalvelho",
-"Chraa",
-"Nature",
-"Deathzomb",
-"Reynandra",
-"Astenari",
-"Noel",
-"Thomaszine",
-"Zeneca",
-"Lethios",
-"Iliketostab",
-"Culazo",
-"Derpshot",
-"Pylorus",
-"Artaios",
-"Molyx",
-"Tervel",
-"Gyurcsány",
-"Feradir",
-"Keti",
-"Llarius",
-"Caty",
-"Chipses",
-"Xunzi",
-"Bônny",
-"Lorance",
-"Sharkness",
-"Elleid",
-"Zeldigo",
-"Polroy",
-"Ballgruf",
-"Hedra",
-"Wargly",
-"Llarius",
-"Facesit",
-"Bed",
-"Anticipating",
-"Chulus",
-"Rukkanen",
-"Venturebro",
-"Tilyn",
-"Painosso",
-"Bokac",
-"Bdc",
-"Gharik",
-"Qiryk",
-"Rabite",
-"Phodom",
-"Doktorka",
-"Powerpomp",
-"Jamesswe",
-"Zeldigohonor",
-"Dwamer",
-"Nudepaper",
-"Naturebound",
-"Cloudbound",
-"Acrimony",
-"Exaspera",
-"Cephalysis",
-"Waterdrinker",
-"Sweps",
-"Pallycakes",
-"Kiraxx",
-"Drexxer",
-"Retfavre",
-"Demonalisa",
-"Painarrow",
-"Araneae",
-"Shanalish",
-"Arcadies",
-"Babybrah",
-"Gnomes",
-"Grilledd"
+"Name1",
+"Name2",
+"Name3"
 }
